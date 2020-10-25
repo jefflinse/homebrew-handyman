a732b223fc1c0aecc48f4e6da10e6f3c245b6d91
Jeff Linse <jeff@jefflinse.com>
2020-10-25 16:16:51 -0700

    Update to Handyman 0.3.0

--- formula/handyman.rb
+++ formula/handyman.rb
@@ -3,8 +3,8 @@ require "formula"
 class Handyman < Formula
   desc "Handyman CLI"
   homepage "https://github.com/jefflinse/handyman"
-  url "https://github.com/jefflinse/handyman/releases/download/v0.2.1/handyman-darwin-amd64-v0.2.1.tar.gz"
-  sha256 "e9e45dc0fd80b529c3d725efead21305b280c3bdf6c2fb69881d63dc00d41812"
+  url "https://github.com/jefflinse/handyman/releases/download/v0.3.0/handyman-darwin-amd64-v0.3.0.tar.gz"
+  sha256 "15eccfa6576328ec11dbc761d4c73132129938ef0d8f4f6a4ea01c513084dff5"
   head "https://github.com/jefflinse/handyman.git"
 
   def install
@@ -12,6 +12,6 @@ class Handyman < Formula
   end
 
   test do
-    assert_match "handyman CLI version v0.2.1", shell_output("#{bin}/hm version", 0)
+    assert_match "handyman CLI version v0.3.0", shell_output("#{bin}/hm version", 0)
   end
 end
